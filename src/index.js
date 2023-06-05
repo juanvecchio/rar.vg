81ab44b7409351e6f029db1d75423bcc8b3c4510
Gerardo Wacker <ge@rar.vg>
2023-06-05 00:08:57 -0300

    Added special routing for subdomains
    Co-authored-by: Juan Ignacio Vecchio <vecchiojuanignacio@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css'
-import { AppHome } from './pages/home'
-import { AppError } from './pages/error'
-import { RouterProvider } from './router/router';
-import { Route } from './router/route';
+import {AppHome} from './pages/home'
+import {AppError} from './pages/error'
+import {RouterProvider} from './router/router';
+import {Route} from './router/route';
+import Profile from "./pages/profile";
+
+// Get the subdomain.
+const host = window.location.host.split('.')
 
 export const WebRoutes = [
     {
@@ -14,21 +18,28 @@ export const WebRoutes = [
     {
         path: "",
         component: <AppError></AppError>,
-		status: 404
+        status: 404
     }
 
 ]
 
-
-
 const AppRoutes = () => <RouterProvider>
-    {WebRoutes.map((route, k) => <Route key={k} status={route.status ? route.status : 200} path={route.path}>{route.component}</Route>)}
+    {WebRoutes.map((route, k) => <Route key={k} status={route.status ? route.status : 200}
+                                        path={route.path}>{route.component}</Route>)}
 </RouterProvider>
 
 
-ReactDOM.render(
-    <AppRoutes></AppRoutes>,
-    document.getElementById("root")
-);
+if (host.length === 2)
+{
+    const subdomain = host[0]
+    ReactDOM.render(<Profile username={subdomain}/>, document.getElementById('root'))
+}
+else
+{
+    ReactDOM.render(
+        <AppRoutes></AppRoutes>,
+        document.getElementById("root")
+    );
+}
 
 export default AppRoutes;
