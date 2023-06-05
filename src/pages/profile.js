b49bc035e304914b74a80f81f629c79adf0b9537
Gerardo Wacker <ge@rar.vg>
2023-06-05 00:08:26 -0300

    Added `Profile` route
    Co-authored-by: Juan Ignacio Vecchio <vecchiojuanignacio@gmail.com>

---
+++ src/pages/profile.js
@@ -0,0 +1,73 @@
+import React from "react";
+import config from '../../config/config.json'
+import ProfileLinks from "../components/profilelinks.component";
+import GenericComponent from "../components/generic.component";
+import PDFComponent from "../components/pdf.component";
+import LinklistComponent from "../components/linklist.component";
+
+import './profile.css'
+import '../index.css'
+
+export default class Profile extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            user: null
+        }
+    }
+
+    componentDidMount()
+    {
+        fetch(config.endpoint.host + '/profile/' + this.props.username)
+            .then(r => r.json())
+            .then(result => this.setState({user: result}))
+    }
+
+    component = (component, key) =>
+    {
+        if (component.type && component.content)
+            switch (component.type)
+            {
+                case "generic":
+                    return <GenericComponent title={component.content.title} description={component.content.description}
+                                             key={key}/>
+                case "pdf":
+                    return <PDFComponent fileId={component.content.fileId} key={key}/>
+                case "linklist":
+                    return <LinklistComponent links={component.content.links} key={key}/>
+            }
+    }
+
+    render()
+    {
+        if (!this.state.user)
+        {
+            return <div>Loading...</div>
+        }
+        return <div className={"content"}>
+            <div className="card">
+                <div className={"header"}>
+                    <div
+                        className={"banner"}
+                    />
+                    <img
+                        className={"profile-picture"}
+                        src={config.endpoint.host + "/avatar/" + this.state.user.id + ".png"}
+                        alt={"Profile picture"}
+                    />
+                    <h1 className={"p-no-margin-bottom"}>{this.state.user.displayName}</h1>
+                    <h3 className={"username p-no-margin-top"}>@{this.props.username}</h3>
+                    <ProfileLinks socials={this.state.user.sociallinks}/>
+                </div>
+
+                {this.state.user.components.map((component, key) => this.component(component, key))}
+
+                <div className={"footer"}>
+                    <a href={"https://rar.vg"} style={{color: "#fff"}}>rar.vg</a> powered 2023
+                </div>
+            </div>
+        </div>
+    }
+}
