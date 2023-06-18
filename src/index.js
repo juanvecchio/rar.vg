1cba55f6139651f6365c86269d9c5d18f914fcdb
Gerardo Wacker <ge@rar.vg>
2023-06-18 03:20:17 -0300

    Prevent short usernames from routing into subdomains

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ const AppRoutes = () => <RouterProvider>
                                         path={route.path}>{route.component}</Route>)}
 </RouterProvider>
 
-if ((host.length === 2 && host[1].includes('localhost')) || (host.length === 3))
+if (((host.length === 2 && host[1].includes('localhost')) || (host.length === 3)) && host[0].length > 3)
 {
     const subdomain = host[0]
     ReactDOM.render(<Profile username={subdomain}/>, document.getElementById('root'))
