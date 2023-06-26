760f60d768d86edac45db2ac18079152fab40e25
Gerardo Wacker <ge@rar.vg>
2023-06-26 11:29:04 -0300

    Added markdown support for `GenericComponent`

--- src/components/generic.component.js
+++ src/components/generic.component.js
@@ -1,12 +1,14 @@
 import React from "react";
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
 
 export default class GenericComponent extends React.Component
 {
     render()
     {
-        return <div className={"component"}>
+        return <div className={this.props.editing ? 'component editing' : 'component'}>
             <h2>{this.props.title}</h2>
-            <h4>{this.props.description}</h4>
+            <h4><ReactMarkdown remarkPlugins={[gfm]}>{this.props.description}</ReactMarkdown></h4>
         </div>
     }
 }
