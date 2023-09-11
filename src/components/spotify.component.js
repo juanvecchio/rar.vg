b4c9abfa42b719b739e648c054641fe48c9513ba
Juan Ignacio Vecchio <vecchiojuanignacio@gmail.com>
2023-09-10 22:00:22 -0300

    Created `SpotifyComponent`

---
+++ src/components/spotify.component.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class SpotifyComponent extends React.Component
+{
+    render()
+    {
+        return <div className={this.props.editing ? 'component editing' : 'component'}>
+            <iframe style={{borderRadius: "12px", pointerEvents: this.props.editing ? "none" : "all"}} src={"https://open.spotify.com/embed/playlist/" + this.props.id}
+            width={"100%"} frameBorder={0} height={"400"} allowFullScreen={true} allow={"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"}  
+            loading={"lazy"}></iframe>
+            </div>
+    }
+}
