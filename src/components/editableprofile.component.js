8d8875962e2f3cb9db13ed550dce84a8e3853904
Gerardo Wacker <ge@rar.vg>
2023-11-16 20:10:47 -0300

    Bump a profile's components amount

--- src/components/editableprofile.component.js
+++ src/components/editableprofile.component.js
@@ -108,7 +108,7 @@ export default class EditableProfile extends React.Component
 
                 <div className={"component add-component-button-container"}>
                     {
-                        this.props.user.components.length >= 3 ? <></> :
+                        this.props.user.components.length >= 5 ? <></> :
                             <button onClick={() => this.toggleModal()} className={"add-component-button"}>
                                 <IoIosAdd size={50}/>
                             </button>
